b83faef1cc98b343d744f982ab2319351afda030
Dina Orucevic <dina.oorucevic@gmail.com>
2023-01-05 22:20:21 -0500

    scrolling reveal animation

---
+++ revealquote2.js
@@ -0,0 +1,18 @@
+function revealquote2() {
+    var reveals = document.querySelectorAll(".revealquote2");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", revealquote2);
+  
---
+++ revealquote3.js
@@ -0,0 +1,18 @@
+function revealquote3() {
+    var reveals = document.querySelectorAll(".revealquote3");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", revealquote3);
+  
